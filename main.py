3a9f2ab237442cfdf22e06ebb4543ff25221ef27
Lukas Manduch <manduch.lukas@gmail.com>
2018-05-10 00:38:51 +0200

    Support for weights

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ import matplotlib.lines
 from matplotlib.lines import Line2D
 import matplotlib.pyplot as plt
 import networkx as nx
+from collections import defaultdict
+import random
+
 
 gr1 = {
     1: [2, 4, 5],
@@ -28,17 +31,48 @@ gr2 = {
     9: [3, 6]
 }
 
-POINTS = list()
+gr3 = { # 20 sided
+    2: [6, 12, 17, 37, 27],
+    6: [2, 12, 17, 31, 33],
+   12: [2, 6, 27, 46, 31],
+   17: [2, 6, 33, 51, 37],
+   27: [2, 12, 37, 54, 46],
+   31: [6, 12, 46, 58, 33],
+   33: [6, 17, 31, 58, 51],
+   37: [2, 17, 27, 51, 54],
+   46: [12, 27, 31, 54, 58],
+   51: [17, 33, 37, 54, 58],
+   54: [27, 37, 46, 51, 58],
+   58: [31, 33, 46, 51, 54]
+   }
+
+gr4 = { # 12 sided
+    4: [8, 11, 13],
+    8: [4, 16, 18],
+    11: [4, 20, 28],
+    13: [4, 30, 23],
+    16: [8, 23, 34],
+    18: [8, 36, 20],
+    20: [11, 18, 38],
+    23: [13, 16, 41],
+    28: [11, 30, 45],
+    30: [13, 28, 47],
+    34: [16, 50, 36],
+    36: [18, 34, 52],
+    38: [20, 45, 52],
+    41: [23, 47, 50],
+    45: [28, 38, 56],
+    47: [30, 41, 56],
+    50: [34, 41, 60],
+    52: [36, 38, 60],
+    56: [45, 47, 60],
+    60: [50, 52, 56]
+}
 
 def add_line(x1, x2):
     x ,y = zip(x1, x2)
     print("Line: " + str(x) + "  " + str(y))
-    global POINTS
-    POINTS.append(x1)
-    POINTS.append(x2)
     plt.plot(x, y)
-    #l = Line2D(x, y)
-    #plt.gca().add_line(l)
 
 def create_circle(num_vertex):
     g = nx.Graph()
@@ -58,10 +92,9 @@ def annotate(positions):
     for label, point in positions.items():
         plt.gca().annotate(label, point)
 
-def solve_for(dimension: int, inp: dict, given:dict):
+def solve_for(dimension: int, inp: dict, given: dict, weights = defaultdict(lambda: 1)):
     equations = np.zeros((len(inp), len(inp)))
     results = np.zeros(len(inp))
-    #index = list(zip(inp.keys(), range(len(inp))))
     index = list(inp.keys())
     ###
     for i in range(len(index)):
@@ -70,11 +103,10 @@ def solve_for(dimension: int, inp: dict, given:dict):
             results[i] = given[value][dimension]
             equations[i][i] = 1
         else:
+            sum_neighbours = sum([weights[x] for x in inp[value]])
             for neighbour in inp[value]:
-                equations[i][index.index(neighbour)] = 1*(1/len(inp[value]))
+                equations[i][index.index(neighbour)] = weights[neighbour]*(1/sum_neighbours)
                 equations[i][i] = -1
-
-    #pprint(equations)
     res = solve(equations, results)
     return res
 
@@ -83,19 +115,20 @@ def tutte(inp: dict, circle_vertices: list):
     positions = dict(zip(circle_vertices, create_circle(len(circle_vertices))))
     pprint(positions)
     np.zeros((1, 3))
-    len(inp)
-    vals = list(zip(solve_for(0, inp, positions),
-                    solve_for(1, inp, positions)))
+    weights = defaultdict(lambda: 8)
+    for key in inp:
+        weights[key] = random.random()
+    vals = list(zip(solve_for(0, inp, positions, weights),
+                    solve_for(1, inp, positions, weights)))
     return dict(zip(inp.keys(), vals))
 
 
 if __name__ == "__main__":
-    pprint(gr1)
-    results = tutte(gr2, [1, 2, 3, 7])
+    graph = gr4
+    circle = [4, 8,  16,  23, 13]
+    results = tutte(graph, circle)
+    draw_lines(results, graph)
     pprint(results)
-    draw_lines(results, gr2)
     annotate(results)
 
     plt.show()
-
-pprint(sorted(POINTS))
